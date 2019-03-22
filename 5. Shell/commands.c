6b6e13fd3645b2ddf8f7e541fd1f8066e9a6db62
Leonardo A. Pérez Castilla <ephelsa@hotmail.com>
2019-03-22 03:07:08 -0500

    Parallel +better code

---
+++ 5. Shell/commands.c
@@ -0,0 +1,8 @@
+//
+//  commands.c
+//  
+//
+//  Created by Leonardo Pérez Castilla on 3/2/19.
+//
+
+#include <stdio.h>
---
+++ 5. Shell/commands.h
@@ -0,0 +1,12 @@
+//
+//  commands.h
+//  
+//
+//  Created by Leonardo Pérez Castilla on 3/2/19.
+//
+
+#ifndef commands_h
+#define commands_h
+
+
+#endif /* commands_h */
