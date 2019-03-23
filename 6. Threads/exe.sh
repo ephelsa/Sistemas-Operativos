dc0b1cd07eb2a05fc5fbf2de79fe848df6383a5a
Leonardo A. Pérez Castilla <ephelsa@hotmail.com>
2019-03-23 00:09:07 -0500

    Files removed

--- 6. Threads/exe.sh
+++ 6. Threads/exe.sh
@@ -8,6 +8,4 @@ gcc-8 -fopenmp main.c mfile/mfile.c merror/merror.c moperation/moperation.c -o m
 echo Script: Executing 
 echo ""
 
-./multiplication_s.out "$1" files/vec_10_6_a.txt files/vec_10_6_b.txt "$2"
-
-#./multiplication_s.out "$1" ../../benchmark/vec_10_8_a.txt ../../benchmark/vec_10_8_b.txt "$2"
+./multiplication_s.out "$1" ../..benchmark/vec_10_6_a.txt ../../benchmark/files/vec_10_6_b.txt "$2"
